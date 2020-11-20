46e84637d82ecc3a2e734289b094136e97ff08e6
Andrea <andrea.bortolossi89@gmail.com>
2020-11-20 21:40:38 +0100

    comment to the code

--- terraform_test_AWS/VPC/main.tf
+++ terraform_test_AWS/VPC/main.tf
@@ -25,12 +25,16 @@ provider "aws" {
 }
 
 ################################################################################
-# - locals;
+# - locals; a local value assigns a name to an expression, so you can use it
+#           multiple times within a module without repeating it (es. local
+#           variable user_tag is used many times in the code to assign tag to
+#           the resources)
 #
 ################################################################################
 locals {
   user_tag = {
-                # Here you can see how to use a variable defined in variable.tf file var.<name-of-the-variable>
+                # Here you can see how to use a variable defined in variable.tf
+                # file var.<name-of-the-variable>
                 Owner = var.awsusername
                 Test  = "VPC"
               }
@@ -43,7 +47,19 @@ locals {
 
 ################################################################################
 # Get the aws ami metadata of and Ubuntu OS image
-# - data;
+#
+# - data; data block allows data to be fetched or computed for use elsewhere in
+#         Terraform configuration. Use of data sources allows a Terraform
+#         configuration to make use of information defined outside of Terraform,
+#         or defined by another separate Terraform configuration.
+#         For example each provider may offer data sources alongside its set of
+#         resource types. In the following block we are going to get infromation
+#         about aws ami owned by aws itself.
+#
+#         Browse terraform documentation online to know what this block is doing
+#         and what kind of input/output it can generate
+#
+#         https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ami
 #
 ################################################################################
 data "aws_ami" "ubuntu" {
@@ -61,7 +77,19 @@ data "aws_ami" "ubuntu" {
 
 ################################################################################
 # Create a key pair to ssh on remote EC2 host
-# - resource;
+#
+# - resource; Each resource block describes one or more infrastructure objects,
+#             such as virtual networks, compute instances, or higher-level
+#             components such as DNS records. A resource block declares a
+#             resource of a given type ("aws_key_pair") with a given local
+#             name ("this"). The name is used to refer to this resource from
+#             elsewhere in the same Terraform module, but has no significance
+#             outside that module's scope.
+#
+#             Browse terraform documentation online to know what this block is
+#             doing and what kind of input/output it can generate
+#
+#             https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair
 #
 ################################################################################
 resource "aws_key_pair" "this" {
@@ -73,8 +101,29 @@ resource "aws_key_pair" "this" {
 
 ################################################################################
 # Create a custom VPC using the terraform module terraform-aws-vpc-master
-# - module;
 #
+# - module; a module is a container for multiple resources that are used
+#           together. Every Terraform configuration has at least one module,
+#           known as its root module, which consists of the resources defined
+#           in the .tf files in the main working directory. A module can call
+#           other modules, which lets you include the child module's resources
+#           into the configuration in a concise way. Modules can also be called
+#           multiple times, either within the same configuration or in separate
+#           configurations, allowing resource configurations to be packaged and
+#           re-used.
+#
+#           The label immediately after the module keyword is a local name,
+#           (es. vpc) which the calling module can use to refer to this instance
+#           of the module (es. module.vpc.vpc_id)
+#
+#           All modules require a source argument, which is a meta-argument
+#           defined by Terraform. Its value is the path to a local directory
+#           containing the module's configuration files (es.
+#           "../../modules_AWS/terraform-aws-vpc-master")
+#
+#           Within the block body (between { and }) are the arguments for the
+#           module. Most of the arguments correspond to input variables defined
+#           by the module (es. name, cidr, azs, ...).
 ################################################################################
 module "vpc" {
   source = "../../modules_AWS/terraform-aws-vpc-master"
@@ -83,11 +132,11 @@ module "vpc" {
   cidr = "10.0.0.0/16"
   azs = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
 
-  private_subnets = ["10.0.0.0/21", "10.0.16.0/21", "10.0.32.0/21"]
-  private_subnet_tags = {subnet_type = "private"}
-
   public_subnets = ["10.0.48.0/21", "10.0.64.0/21", "10.0.80.0/21"]
   public_subnet_tags = {subnet_type = "public"}
+
+  private_subnets = ["10.0.0.0/21", "10.0.16.0/21", "10.0.32.0/21"]
+  private_subnet_tags = {subnet_type = "private"}
 
   database_subnets = ["10.0.96.0/21", "10.0.112.0/21", "10.0.128.0/21"]
   database_subnet_tags = {subnet_type = "database"}
@@ -108,10 +157,12 @@ module "vpc" {
 }
 
 ################################################################################
-# Create Route53 host zone and add some DNS records
+# Create Route53 host zone
 #
-#
-#
+# Here again we are using the "resource" terraform block to create a route53
+# hosted zone. Hosted zone should be associated to a VPC to well resolve DNS
+# names. To assign the vpc we need the id and we can get it from the vpc module
+# that we created before.
 #
 ################################################################################
 resource "aws_route53_zone" "private" {
@@ -123,26 +174,51 @@ resource "aws_route53_zone" "private" {
   tags = local.user_tag
 }
 
+################################################################################
+# Create DNS record for private EC2 instance
+#
+# We are using the resource block to create a route53 record.
+# The record must be added to a host zone. We choose the host zone created within
+# this .tf file (aws_route53_zone.private.zone_id).
+# The friendly name ("private.example.com") is associated with the private ip
+# address of the ec2 that we are going to create later in the code
+# (module.ec2_private.private_ip[0])
+#
+################################################################################
 resource "aws_route53_record" "private" {
   zone_id = aws_route53_zone.private.zone_id
   name    = "private.example.com"
   type    = "A"
   ttl     = "300"
-  records = ["${module.ec2_private.private_ip[0]}"]
+  records = [module.ec2_private.private_ip[0]]
 }
 
+################################################################################
+# Create DNS record for database EC2 instance
+#
+# Here we are adding the DNS record for the EC2 instance database. Same as the
+# previous resource but we are calling the other ec2 instance
+# (module.ec2_database.private_ip[0])
+#
+################################################################################
 resource "aws_route53_record" "database" {
   zone_id = aws_route53_zone.private.zone_id
   name    = "database.example.com"
   type    = "A"
   ttl     = "300"
-  records = ["${module.ec2_database.private_ip[0]}"]
+  records = [module.ec2_database.private_ip[0]]
 }
 
 ################################################################################
 # Create an EC2 instances using the terraform module terraform-aws-ec2-instance-master
 #
-#
+# We are using a different module to create an ec2 instance. To set several
+# variables we are using many references to resources or modules created in this
+# .tf file:
+# - data.aws_ami.ubuntu.id; ami id got from AWS
+# - aws_key_pair.this.key_name; created with our id_rsa.pub
+# - module.aws_security_group_server.this_security_group_id; security group created later in the code
+# - tolist(module.vpc.public_subnets)[0]; associate the first public subnet of the VPC
 ################################################################################
 module "ec2_public" {
   source                      = "../../modules_AWS/terraform-aws-ec2-instance-master"
@@ -163,6 +239,9 @@ module "ec2_public" {
 ################################################################################
 # Create an EC2 instances using the terraform module terraform-aws-ec2-instance-master
 #
+# We are using the same module as before but we are deploying a new EC2
+# instance on the private subnet of the first azs
+# - tolist(module.vpc.private_subnets)[0]
 #
 ################################################################################
 module "ec2_private" {
@@ -183,6 +262,9 @@ module "ec2_private" {
 ################################################################################
 # Create an EC2 instances using the terraform module terraform-aws-ec2-instance-master
 #
+# We are using the same module as before but we are deploying a new EC2
+# instance on the database subnet of the first azs
+# - tolist(module.vpc.database_subnets)[0]
 #
 ################################################################################
 module "ec2_database" {
@@ -203,6 +285,9 @@ module "ec2_database" {
 ################################################################################
 # Create a security group using the terraform module terraform-aws-security-group-master
 #
+# We are using a different module to create a security group. We have to assign
+# the security group to a vpc (module.vpc.vpc_id) and we are defining some useful
+# rules for inbound and outbound trafic.
 #
 ################################################################################
 module "aws_security_group_server" {
